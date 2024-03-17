b6dc8bb820ada7d355e2977f5031cd302ba051f3
Tyler Roberts <tylergordonr@gmail.com>
2024-03-17 10:30:46 -0400

    added mock api call and rearranged existing profile page

--- src/features/profile/ProfilePage.js
+++ src/features/profile/ProfilePage.js
@@ -2,18 +2,19 @@ import React from "react";
 import ProfileImageCoursel from "./ProfileImageCarousel";
 import { Container, Row, Col } from "react-bootstrap";
 
-const ProfilePage = ({name, value}) => {
+const ProfilePage = () => {
 
     return (
         <Container fluid>
-            <Row className="justify-content-center">
-                <Col className="text-center">
-                    <h1>John Cena Profile</h1>
-                </Col>
-            </Row>
             <Row className="align-items-center">
                 <Col xs="auto" className="mx-auto">
                     <ProfileImageCoursel imagesIds={[]}/>
+                </Col>
+            </Row>
+
+            <Row className="justify-content-center">
+                <Col className="text-center">
+                    <h1>John Cena</h1>
                 </Col>
             </Row>
         </Container>
